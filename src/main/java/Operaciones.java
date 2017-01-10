c964b7c49f47c8f0e969add7f9afcc1ae79d3c8e
Juan Antonio <juanancastacor@hotmail.com>
2017-01-10 11:36:46 +0100

    Implementación de media para dos valores cualquieras iguales

--- src/main/java/Operaciones.java
+++ src/main/java/Operaciones.java
@@ -5,6 +5,6 @@ public class Operaciones
   public static Double media(Double d1, Double d2) 
   {
 	
-     return 5.0;
+     return d1;
      }
 }
--- src/test/java/MediaTest.java
+++ src/test/java/MediaTest.java
@@ -5,17 +5,30 @@ import org.junit.Test;
 
 import main.java.Operaciones;
 
-public class MaquinaExpendedoraTest {
+public class MediaTest {
     
     @Test
-    public void testCorriente(){
+    public void testMedia1(){
      
          Assert.assertTrue(5.0 == Operaciones.media(5.0,5.0));
     }
+    
+    @Test
+    public void testMedia2(){
+     
+         Assert.assertTrue(2.0 == Operaciones.media(2.0,2.0));
+    }
+    
+    @Test
+    public void testMedia3(){
+     
+         Assert.assertTrue(3.0 == Operaciones.media(3.0,3.0));
+    }
+
 
     
     @Test(expected = AssertionError.class)
-    public void testCrearMaquinaNumerosNegativos(){
+    public void testMediaNegativo(){
         
     	 Assert.assertTrue(0.0 == Operaciones.media(5.0,5.0));
     }
